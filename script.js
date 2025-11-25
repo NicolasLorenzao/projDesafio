46377f03504bf3776b02c76c97fd9ef36066f26c
Nicolas Lorenzão <nicolas.lorenzao@escola.pr.gov.br>
2025-11-25 08:48:46 -0300

    ultima versao

--- script.js
+++ script.js
@@ -1,15 +1,122 @@
-let botaoMaria = document.getElementById("botaoMaria");
-let descMaria = document.getElementById("descMaria");
+const alunos = [
+  {
+    idAluno: "aluno1",
+    descricao: "",
+  },
+  {
+    idAluno: "aluno2",
+    descricao: "",
+  },
+  {
+    idAluno: "aluno3",
+    descricao: "",
+  },
+  {
+    idAluno: "aluno4",
+    descricao: "",
+  },
+  {
+    idAluno: "aluno5",
+    descricao: "",
+  },
+  {
+    idAluno: "aluno6",
+    descricao: "",
+  },
+  {
+    idAluno: "aluno9",
+    descricao: "",
+  },
+  {
+    idAluno: "aluno10",
+    descricao: "",
+  },
+  {
+    idAluno: "aluno11",
+    descricao: "",
+  },
+  {
+    idAluno: "aluno12",
+    descricao: "",
+  },
+  {
+    idAluno: "aluno13",
+    descricao: "",
+  },
+  {
+    idAluno: "aluno14",
+    descricao: "",
+  },
+  {
+    idAluno: "aluno15",
+    descricao: "",
+  },
+  {
+    idAluno: "aluno16",
+    descricao: "",
+  },
+  {
+    idAluno: "aluno17",
+    descricao: "",
+  },
+  {
+    idAluno: "aluno18",
+    descricao: "",
+  },
+  {
+    idAluno: "aluno19",
+    descricao: "",
+  },
+  {
+    idAluno: "aluno20",
+    descricao: "",
+  },
+  {
+    idAluno: "aluno21",
+    descricao: "",
+  },
+  {
+    idAluno: "aluno22",
+    descricao: "",
+  },
+  {
+    idAluno: "aluno23",
+    descricao: "",
+  },
+  {
+    idAluno: "aluno25",
+    descricao: "",
+  },
+  {
+    idAluno: "aluno26",
+    descricao: "",
+  },
+  {
+    idAluno: "aluno29",
+    descricao: "",
+  },
+  {
+    idAluno: "aluno30",
+    descricao: "",
+  },
+  {
+    idAluno: "aluno31",
+    descricao: "",
+  },
+  {
+    idAluno: "aluno32",
+    descricao: "",
+  },
+];
 
-botaoMaria.addEventListener("click", acao);
-
-function acao(){
-    descMaria.classList.toggle("desativa");
-}
-
-let botaoLuiz = document.getElementById("botaoLuiz");
-let descLuiz = document.getElementById("descLuiz");
-
-botaoLuiz.addEventListener("click", function () {
-    descLuiz.classList.toggle("ddesativa");
+document.addEventListener("DOMContentLoaded", function () {
+  document.getElementById();
 });
+
+let paragrafo = document.getElementById("descricao");
+
+let aluno = 0;
+
+function botao(aluno) {
+  paragrafo.innerHTML = alunos[aluno - 1]["descricao"];
+}
